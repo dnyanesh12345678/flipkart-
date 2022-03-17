5f432dfbe15f07a6c6a094084349a42a786181c8
dnyanesh12345678 <9dnyanesh@gmail.com>
2022-03-17 21:24:57 +0530

    env addes

---
+++ env.sh
@@ -0,0 +1,17 @@
+
+
+#!/bin/bash
+cont_count=$1
+echo "creating $cont_count containers.."
+sleep 2;
+for i in `seq $cont_count`
+do
+	echo "=============================="
+    echo "Creating tomcat-server$i container.."
+    sleep 1
+    docker run -it -d --name tomcat-server$i 9dnyanesh/iflipkart-image
+    echo "tomcat-server$i container has been created!"
+	echo "=============================="
+done
+docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -q` > IPs.txt
+
